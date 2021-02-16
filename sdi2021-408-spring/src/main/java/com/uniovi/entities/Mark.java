31148c85a5f74b209bf86ec2d7f4ae838233dee1
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-02-16 23:46:35 +0100

    SDI-2021-408-2.4-Modelo, acceso a datos simple.

--- sdi2021-408-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi2021-408-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -1,7 +1,13 @@
 package com.uniovi.entities;
 
-public class Mark {
-	//Lista de atributos:
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Mark {	
+	@Id
+	@GeneratedValue
 	private long id;
 	private String description;
 	private Double score;
