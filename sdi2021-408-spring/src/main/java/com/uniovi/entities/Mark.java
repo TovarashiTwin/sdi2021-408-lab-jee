0a5606a383eeee323c09505d2d29f4d2a2ab09de
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-02-16 22:33:52 +0100

    SDI-2021-101-2.2-Trabajando con Controladores.

---
+++ sdi2021-408-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -0,0 +1,30 @@
+package com.uniovi.entities;
+
+public class Mark {
+	//Lista de atributos:
+	private long id;
+	private String description;
+	private Double score;
+
+	//Métodos get y setters, autogenerado.
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getScore() {
+		return score;
+	}
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+
+}
