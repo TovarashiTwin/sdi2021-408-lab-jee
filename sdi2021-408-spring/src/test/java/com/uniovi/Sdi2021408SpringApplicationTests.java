87ecd8890bae64825ab5e73dd6d6f7c89794e5dd
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-02-16 17:12:26 +0100

    SDI-2021-408-2.1-Create Spring project.

---
+++ sdi2021-408-spring/src/test/java/com/uniovi/Sdi2021408SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2021408SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
