84fc61cb81c9f148945cc00dae23c7f72c1d9a3a
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-02-23 22:01:48 +0100

    SDI-2021-408-3.4-Autenticación y control de acceso.

--- sdi2021-408-spring/src/main/java/com/uniovi/entities/User.java
+++ sdi2021-408-spring/src/main/java/com/uniovi/entities/User.java
@@ -8,8 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
+@Table(name="user")
 public class User {
 
 	@Id
@@ -20,6 +23,10 @@ public class User {
 	private String name;
 	private String lastName;
 	private String role;
+	
+	private String password;
+	@Transient//propiedad que no se almacena e la tabla. 
+	private String passwordConfirm;
 
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	private Set<Mark> marks;
@@ -86,5 +93,22 @@ public class User {
 	public void setMarks(Set<Mark> marks) {
 		this.marks = marks;
 	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+	
 
 }
