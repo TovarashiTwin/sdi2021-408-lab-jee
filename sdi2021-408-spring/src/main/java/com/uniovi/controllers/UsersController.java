1cd1b1203882fdadea2cc9ed06735b39a474bd38
TovarashiTwin <tecmanuellor1516@gmail.com>
2021-03-09 23:00:42 +0100

    SDI-2021-408-Arreglo ejercicio complementario 1

--- sdi2021-408-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi2021-408-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -1,6 +1,11 @@
 package com.uniovi.controllers;
 
+import java.util.LinkedList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -12,7 +17,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.uniovi.entities.Mark;
 import com.uniovi.entities.User;
+import com.uniovi.services.MarksService;
 import com.uniovi.services.RolesService;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
@@ -22,6 +29,9 @@ import com.uniovi.validators.SignUpFormValidator;
 public class UsersController {
 	@Autowired
 	private UsersService usersService;
+	
+	@Autowired
+	private MarksService marksService;
 
 	@Autowired
 	private SecurityService securityService;
@@ -108,11 +118,13 @@ public class UsersController {
 	}
 
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
-	public String home(Model model) {
+	public String home(Model model,Pageable pageable) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		String dni = auth.getName();
 		User activeUser = usersService.getUserByDni(dni);
-		model.addAttribute("markList", activeUser.getMarks());
+		Page<Mark> marks = new PageImpl<Mark>(new LinkedList<Mark>());
+		marks = marksService.getMarksForUser(pageable, activeUser);
+		model.addAttribute("markList", marks);
 		return "home";
 	}
 }
